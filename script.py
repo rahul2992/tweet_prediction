951c88c263b10ab3f242d4a523ba9e53f21c419e
rahul2992 <rahuljainnsit9@gmail.com>
2017-03-07 20:05:09 +0530

    Add intertweet time and histogram

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
 def load_train():
 	file = "new_gruber_tweets.json"
@@ -8,4 +9,21 @@ def load_train():
 	
 if __name__ == '__main__':
 	tweetdata = load_train()
-	print tweetdata 
+	tweetdata['created_at'] = pd.to_datetime(tweetdata['created_at'])
+	
+	tweet_time = []
+	for time in tweetdata['created_at']:
+		tweet_time.append(time)
+	tweet_time.sort()
+	
+	time_dif = []
+	for i in xrange(1, len(tweet_time) - 1):
+		temp = (tweet_time[i] - tweet_time[i - 1]).seconds
+		time_dif.append(temp)
+		
+	time_df = pd.Series(time_dif)
+	time_df.hist(bins = 30, normed = True)
+	plt.show()
+	
+	
+	
